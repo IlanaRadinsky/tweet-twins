9da777ab49d3a90d7eb77f007550b3a10659e0c6
Ilana Radinsky <ilanaradinsky@gmail.com>
2018-05-03 21:34:15 -0400

    first script to download tweets

---
+++ get_tweets.py
@@ -0,0 +1,28 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import pprint
+
+ckey = '9iYEHY0pfWIGQ8YFduNGbedQv'
+csecret = 'u9btwE95BSaqobp88lmPYDup2fwFIByqAGEOTbRcBQoRzed4Ym'
+
+atoken = '978265942668054528-3OVrymkyZPXMMUVnEDOIpnA6KUBU92T'
+asecret = 'qO3OmfXJK61bBZStpUw5F3ZbPKhK1gZ3Zlim4OO2DaGJ5'
+
+class listener(StreamListener):
+    def on_data(self, data):
+        pp = pprint.PrettyPrinter()
+        data = data.strip()
+        #print(data)
+        pp.pprint(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+
+twitterStream.filter(track=["torah", "hashem", "yeshiva", "kosher"])
