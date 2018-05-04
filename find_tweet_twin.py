f4aad1ae1d45dfc781edefee8b729cc4023e8c34
Ilana Radinsky <ilanaradinsky@gmail.com>
2018-05-04 04:34:50 -0400

    Trying to match user to friends

--- find_tweet_twin.py
+++ find_tweet_twin.py
@@ -4,7 +4,7 @@ f = open("results200-1.txt", "r")
 
 users = {}
 
-lines = f.readline()
+lines = f.readlines()
 for l in lines:
     ans = l.split("|")
     user_name = ans[0]
@@ -21,6 +21,8 @@ f = open("hashtagUsers.txt", "r")
 
 hashtagUsers = {}
 prev = None
+
+lines = f.readlines()
 for l in lines:
     ans = l.split("|")
     hashtag = ans[0]
@@ -32,13 +34,36 @@ for l in lines:
     else:
         hashtagUsers[hashtag] = [(float(perc), user_name)]
 
-    if hashtag != prev:
+    if prev and hashtag != prev:
         hashtagUsers[prev].sort()
 
     prev = hashtag
     
 f.close()
 
-pp = PrettyPrinter(indent=4)
-pp.pprint(users)
-pp.pprint(hashtagUsers)
+#pp = pprint.PrettyPrinter(indent=4)
+#print("*************USERS******************")
+#pp.pprint(users)
+#print("************HASHTAGS****************")
+#pp.pprint(hashtagUsers)
+
+searchUser = input("Input a user to find his/her friends!")
+twinTweeters = {}
+
+for hashtag in users[searchUser]:
+    usage = hashtag[0]
+    for targetUser in hashtagUsers[hashtag[1]]:
+        targetUsage = targetUser[0]
+        diff = abs(usage-targetUsage)
+        if targetUser[1] != hashtag[1] and diff < 0.05:
+            if targetUser[1] in twinTweeters:
+                twinTweeters[targetUser[1]] += usage/(1+diff) # the larger the difference between the perc
+                                                          # vals, the smaller your rank will be. the
+                                                          # larger the usage percentage, the more your
+                                                          # rank will be weighted.
+            else:
+                twinTweeters[targetUser[1]] = usage/(1+diff)
+
+twinTweetersList = [(v,k) for k, v in twinTweeters.items()]
+twinTweetersList.sort(reverse=True)
+print(twinTweetersList)            
