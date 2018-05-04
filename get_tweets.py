0439a2f5934c00c6d4158ca2617477c5983dbf45
Ilana Radinsky <ilanaradinsky@gmail.com>
2018-05-04 00:49:16 -0400

    Parse tweets and extract text and username

--- get_tweets.py
+++ get_tweets.py
@@ -10,23 +10,48 @@ csecret = 'u9btwE95BSaqobp88lmPYDup2fwFIByqAGEOTbRcBQoRzed4Ym'
 atoken = '978265942668054528-3OVrymkyZPXMMUVnEDOIpnA6KUBU92T'
 asecret = 'qO3OmfXJK61bBZStpUw5F3ZbPKhK1gZ3Zlim4OO2DaGJ5'
 
+global results
+results = {}
+
 class listener(StreamListener):
-    def on_data(self, data):
+    def __init__(self, api=None):
+        super(listener, self).__init__()
+        self.__numTweets = 0
+       
+    #def on_data(self, data):
+    def ignore():
+        global results
         pp = pprint.PrettyPrinter(indent=4)
 
         data = data.strip()
         d = json.loads(data)
         if d['lang'] == 'en':
             #pp.pprint(d)
-            print(d['text'], d['user']['id'], d['user']['screen_name'])
+            text = d['text']
+            user_id = d['user']['id']
+            screen_name = d['user']['screen_name']
+            print(text, user_id, screen_name)    
         return True
 
     def on_error(self, status):
         print(status)
+
+    def on_status(self, status):
+        text = status.text
+        screen_name = status.user.screen_name
+        if screen_name in results:
+                results[screen_name].append(text)
+        else:
+                results[screen_name] = [text]
+        self.__numTweets += 1
+        return self.__numTweets < 10
 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 
 twitterStream = Stream(auth, listener())
 
-twitterStream.filter(track=["torah", "hashem", "yeshiva", "kosher"])
+twitterStream.filter(track=["#"])
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(results)
